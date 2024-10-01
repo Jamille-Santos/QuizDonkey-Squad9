e686bd605349ad4e13e165d140c8951987815ffa
Guilherme-lgs <melloguilerme38@gmail.com>
2024-09-30 21:58:51 -0300

    add perguntas

---
+++ js/perguntas.js
@@ -0,0 +1,37 @@
+let quizdata=[
+    {
+
+        question:"Quem é o arqui-inimigo do Batman?",
+        opcao:["Lex Luthor","Coringa","Duas-Caras","Thanos"],
+        correto:"Coringa",
+
+},
+{
+
+    question:"Qual é o nome do lar dos super-heróis da Marvel conhecido como Vingadores ",
+    opcao:["Asgard","Xandar","Torre dos Vingadores","Gothan City"],
+    correto:"Torre dos Vingadores",
+
+},
+{
+
+    question:"Qual é o super-herói que tem a habilidade de escalar paredes e lançar teias? ",
+    opcao:["Flash"," Homem-Aranha","Wolverine"," Lanterna Verde"],
+    correto:"Homem-Aranha",
+
+},
+{
+
+    question:"Qual super-herói é conhecido por seu escudo indestrutível?",
+    opcao:["Capitão América" ,"Aquaman","Demolidor","Hulk"],
+    correto:"Capitão América",
+
+},
+{
+
+    question:"Qual é o verdadeiro nome do anti-herói conhecido como Deadpool?",
+    opcao:["Peter Parker","Wade Wilson"," Oliver Queen","Frank Castle"],
+    correto:"Wade Wilson",
+
+},
+];
